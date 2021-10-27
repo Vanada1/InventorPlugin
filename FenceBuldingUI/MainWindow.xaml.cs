72ce664ebcb61fbc601b7421886f1758a11bc033
Vladimir Shvoev <shvoev.work@gmail.com>
2021-10-27 15:51:53 +0700

    Исправление Проекта системы

--- FenceBuldingUI/MainWindow.xaml.cs
+++ FenceBuldingUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace FenceBuldingUI
 		{
 			InitializeComponent();
 		}
+
+		private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show("Есть ошибки. Проверьте!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
 	}
 }
