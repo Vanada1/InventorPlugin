19e9f07656cfb5c000b402b76ffed1f13c783f00
Vladimir Shvoev <shvoev.work@gmail.com>
2021-11-15 19:00:05 +0700

    Добавлено построение всего забора

--- InventorApi/FenceBuilder.cs
+++ InventorApi/FenceBuilder.cs
@@ -23,6 +23,15 @@ namespace InventorApi
 
 		#endregion
 
+		#region -- Properties --
+
+		/// <summary>
+		/// Координата средней палки.
+		/// </summary>
+		private double MiddleStickY => _fenceParameters.FenceHeight - 2 * _fenceParameters.ColumnWidth;
+
+		#endregion
+
 		#region -- Constructors --
 
 		/// <summary>
@@ -63,7 +72,6 @@ namespace InventorApi
 			var fenceLength = _fenceParameters.FenceLength;
 			var fenceHeight = _fenceParameters.FenceHeight;
 			var immersionDepth = _fenceParameters.ImmersionDepth;
-			var middleStickY = fenceHeight - 2 * columnWidth;
 			var points = new List<Point2d>
 			{
 				_inventorWrapper.TransientGeometry.CreatePoint2d(0, 0),
@@ -74,8 +82,8 @@ namespace InventorApi
 				_inventorWrapper.TransientGeometry.CreatePoint2d(columnWidth, immersionDepth),
 				_inventorWrapper.TransientGeometry.CreatePoint2d(fenceLength - columnWidth,
 					immersionDepth + columnWidth),
-				_inventorWrapper.TransientGeometry.CreatePoint2d(columnWidth, middleStickY),
-				_inventorWrapper.TransientGeometry.CreatePoint2d(fenceLength - columnWidth, middleStickY - columnWidth),
+				_inventorWrapper.TransientGeometry.CreatePoint2d(columnWidth, MiddleStickY),
+				_inventorWrapper.TransientGeometry.CreatePoint2d(fenceLength - columnWidth, MiddleStickY - columnWidth),
 			};
 
 			var sketchXy = _inventorWrapper.MakeNewSketch(1, 0);
@@ -89,12 +97,21 @@ namespace InventorApi
 				sketchXy.SketchLines.AddAsTwoPointRectangle(points[7],points[8]),
 			};
 
-			var sketchProfile = sketchXy.Profiles.AddForSolid();
+			Extrude(sketchXy);
+		}
+
+		/// <summary>
+		/// Выдавливание.
+		/// </summary>
+		/// <param name="sketch">Эскиз.</param>
+		private void Extrude(PlanarSketch sketch)
+		{
+			var sketchProfile = sketch.Profiles.AddForSolid();
 
 			var extrudeDef =
 				_inventorWrapper.PartDefinition.Features.ExtrudeFeatures.CreateExtrudeDefinition(sketchProfile,
 					PartFeatureOperationEnum.kJoinOperation);
-			extrudeDef.SetDistanceExtent(columnWidth, PartFeatureExtentDirectionEnum.kSymmetricExtentDirection);
+			extrudeDef.SetDistanceExtent(_fenceParameters.ColumnWidth, PartFeatureExtentDirectionEnum.kSymmetricExtentDirection);
 			var extrude = _inventorWrapper.PartDefinition.Features.ExtrudeFeatures.Add(extrudeDef);
 			var objectCollection = _inventorWrapper.CreateObjectCollection();
 			objectCollection.Add(extrude);
@@ -105,7 +122,13 @@ namespace InventorApi
 		/// </summary>
 		private void BuildLowerInnerPart()
 		{
+			var columnWidth = _fenceParameters.ColumnWidth;
+			var fenceHeight = _fenceParameters.FenceHeight;
+			var immersionDepth = _fenceParameters.ImmersionDepth;
+			var y1 = immersionDepth + columnWidth;
+			var y2 = MiddleStickY - columnWidth;
 
+			BuildInnerPart(y1, y2, _fenceParameters.DistanceLowerBaffles);
 		}
 
 		/// <summary>
@@ -113,7 +136,44 @@ namespace InventorApi
 		/// </summary>
 		private void BuildUpperInnerPart()
 		{
+			var columnWidth = _fenceParameters.ColumnWidth;
+			var fenceHeight = _fenceParameters.FenceHeight;
+			var y1 = MiddleStickY;
+			var y2 = fenceHeight - columnWidth;
 
+			BuildInnerPart(y1, y2, _fenceParameters.DistanceUpperBaffles);
+		}
+
+		/// <summary>
+		/// Создает внутренние верхние и нижние части забора.
+		/// </summary>
+		/// <param name="y1">Первое начальное положение по Y.</param>
+		/// <param name="y2">Второе начальное положение по Y.</param>
+		/// <param name="distance">Расстояние между прутьями.</param>
+		private void BuildInnerPart(double y1, double y2, double distance)
+		{
+			var columnWidth = _fenceParameters.ColumnWidth;
+			var fenceLength = _fenceParameters.FenceLength;
+			var deltaX = columnWidth + distance;
+
+			var currentPoint1 = _inventorWrapper.TransientGeometry.CreatePoint2d(deltaX, y1);
+			var currentPoint2 = _inventorWrapper.TransientGeometry.CreatePoint2d(deltaX + columnWidth, y2);
+			var sketchXy = _inventorWrapper.MakeNewSketch(1, 0);
+			var rectangles = new List<SketchEntitiesEnumerator>();
+
+			while (fenceLength - columnWidth - currentPoint2.X > columnWidth)
+			{
+				rectangles.Add(sketchXy.SketchLines.AddAsTwoPointRectangle(currentPoint1,
+					currentPoint2));
+				currentPoint1 =
+					_inventorWrapper.TransientGeometry.CreatePoint2d(currentPoint2.X + distance,
+						currentPoint1.Y);
+				currentPoint2 =
+					_inventorWrapper.TransientGeometry.CreatePoint2d(currentPoint1.X + columnWidth,
+						currentPoint2.Y);
+			}
+
+			Extrude(sketchXy);
 		}
 
 		#endregion
