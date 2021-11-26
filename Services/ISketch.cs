739f86d59f0d449abb70b9f299cb279b13e96427
Vladimir Shvoev <shvoev.work@gmail.com>
2021-11-26 07:46:27 +0700

    Добавлены интерфейсы

---
+++ Services/ISketch.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Services
+{
+	/// <summary>
+	/// Интерфейс эскиза.
+	/// </summary>
+	public interface ISketch
+	{
+		/// <summary>
+		/// Построение на эскизе прямоугольника через две точки.
+		/// </summary>
+		/// <param name="point1">Первая точка.</param>
+		/// <param name="point2">Вторая точка.</param>
+		void CreateRectangleByTwoPoint(Point point1, Point point2);
+	}
+}
