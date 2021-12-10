ff515291837309e8de84df49ee54a22c7d9b9735
Dyagay Alexandra <dyagaay@gmail.com>
2021-12-10 17:05:24 +0700

    Добавлен проект теста нагрузки

---
+++ srs/StressTesting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Builder;
+using Core;
+using InventorApi;
+using KompasApi;
+using Services;
+
+namespace StressTesting
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestInventor();
+			TestKompas3D();
+		}
+
+		private static void TestInventor()
+		{
+			TestApi(new InventorWrapper());
+		}
+
+		private static void TestKompas3D()
+		{
+			TestApi(new KompasWrapper());
+		}
+
+		private static void TestApi(IApiService apiService)
+		{
+			var builder = new FenceBuilder();
+			var stopWatch = new Stopwatch();
+			stopWatch.Start();
+			var fenceParameters = new FenceParameters();
+			var streamWriter = new StreamWriter($"log{apiService}.txt", true);
+			var count = 0;
+			var stopCount = 40;
+			while (count < stopCount)
+			{
+				builder.BuildFence(fenceParameters, apiService);
+				streamWriter.WriteLine($"{++count} -- {stopWatch.Elapsed:hh\\:mm\\:ss}");
+			}
+			stopWatch.Stop();
+
+			Console.Write($"End {stopWatch.Elapsed}");
+		}
+	}
+}
