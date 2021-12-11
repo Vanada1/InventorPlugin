9311916d2a6e2903f065bfcbed715f79972babe8
Vladimir Shvoev <shvoev.work@gmail.com>
2021-12-11 12:38:34 +0700

    Добавлен класс параметра

---
+++ src/Core/Parameter.cs
@@ -0,0 +1,49 @@
+﻿namespace Core
+{
+	/// <summary>
+	/// Класс параметра.
+	/// </summary>
+	public class Parameter
+	{
+		/// <summary>
+		/// Значение параметра.
+		/// </summary>
+		private double _value;
+
+		/// <summary>
+		/// Возвращает максимальное значение.
+		/// </summary>
+		public double MaxValue { get; }
+
+		/// <summary>
+		/// Возвращает минимальное значение.
+		/// </summary>
+		public double MinValue { get; }
+
+		/// <summary>
+		/// Возвращает и устанавливает значение параметра.
+		/// </summary>
+		public double Value
+		{
+			get => _value;
+			set
+			{
+				_value = value;
+				Validator.Validate(_value, MinValue, MaxValue);
+			}
+		}
+
+		/// <summary>
+		/// Конструктор.
+		/// </summary>
+		/// <param name="minValue">Минимальное значение.</param>
+		/// <param name="maxValue">Максимальное значение.</param>
+		/// <param name="value">Значение параметра.</param>
+		public Parameter(double minValue, double maxValue, double value)
+		{
+			MinValue = minValue;
+			MaxValue = maxValue;
+			Value = value;
+		}
+	}
+}
